c6195a6d9e268f3486b010f52cbdf1cad9920e3a
narayan6660 <bisoyeenarayan@gmail.com>
2026-08-19 00:07:40 +0530

    Add Company Policy DocType schema

---
+++ access_custom/access_custom/doctype/company_policy/company_policy.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Charan and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class CompanyPolicy(Document):
+	pass
